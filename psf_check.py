ed15bc5f027f0cf9476cde088ad23f848712ea50
jradcliffe5 <jack.radcliffe@postgrad.manchester.ac.uk>
2017-10-05 12:15:00 +0200

    change

--- psf_check.py
+++ psf_check.py
@@ -1,11 +1,12 @@
 import os, sys
 
-scale = [13]
+scale = [0.01,0.1,10,100,10000]
 
 for i in range(len(scale)):
-	os.system('rsync -ar --progress UV_data/*ms ./')
+	#os.system('rsync -ar --progress UV_data/*ms ./')
 	for file in os.listdir('./'):
-		if file.endswith('.ms') and file.startswith('J123642_621331_uvfix'):
-			os.system('casa-release-4.7.1-el7/bin/casa --nologger --log2term -c wt_mod_CASAv1.py scale '+file+' '+str(scale[i]))
-	os.system('casa-release-4.7.1-el7/bin/mpicasa -n 24 casa-release-4.7.1-el7/bin/casa --nologger --log2term -c tclean.py '+str(scale[i]))
+		print file
+		if file.endswith('.ms') and file.startswith('17JUL2015'):
+			os.system('casa-release-5.1.0-74.el7/bin/casa --nologger --log2term -c wt_mod_CASAv1.py scale '+file+' '+str(scale[i]))
+	os.system('casa-release-5.1.0-74.el7/bin/mpicasa -n 24 casa-release-5.1.0-74.el7/bin/casa --nologger --log2term -c tclean.py '+str(scale[i]))
 	os.system('rm *log')
