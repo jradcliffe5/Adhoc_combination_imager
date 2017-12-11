e15f9083a43c1827d2e1789b4c851560844156aa
jradcliffe5 <jack.radcliffe@postgrad.manchester.ac.uk>
2017-12-11 11:35:33 +0100

    generalised fcns

--- psf_check.py
+++ psf_check.py
@@ -1,14 +1,19 @@
 import os, sys
 
-scale = [1.4]
+### Inputs
+scale = [1]
+path_to_ms = '../'
+path_to_casa = '../casa-release-5.1.1-5.el7/bin/'
+ms1 = 'eMER_L_25JUL2015_UVFIX_PEELED.ms'
+ms2 = 'eMER_L_17JUL2015_UVFIX_PEELED.ms'
 
+os.system('rsync -ar --progress %s%s ./' % (path_to_ms,ms2))
 for i in range(len(scale)):
-	os.system('rsync -ar --progress UV/*ms ./')
+	os.system('rsync -ar --progress %s%s ./' % (path_to_ms,ms1))
 	for file in os.listdir('./'):
-		if file.endswith('.ms') and file.startswith('merge_vla'):
-			print 'scaling by %.2f' % scale[i]
-			os.system('casa-release-5.1.0-74.el7/bin/casa --nologger --log2term -c wt_mod_CASAv2.py scale '+file+' '+str(scale[i]))
-	
-	#os.system('casa-release-5.1.0-74.el7/bin/mpicasa -n 24 casa-release-5.1.0-74.el7/bin/casa --nologger --log2term -c tclean.py '+str(scale[i]))
+		if file==ms1:
+			print 'scaling %s by %.2f' % (ms1,scale[i])
+			os.system('%scasa --nologger --log2term -c wt_mod_CASAv2.py scale %s %s' % (path_to_casa,file, str(scale[i])))
+
+	os.system('%smpicasa -n 24 %scasa --nologger --log2term -c tclean.py %s %s %s' % (path_to_casa,path_to_casa,str(scale[i]),ms1,ms2))
 	os.system('rm *log')
-	
