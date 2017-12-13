2da7ec4cd16c32614c47fd2d014ce26862c6a724
jradcliffe5 <jack.radcliffe@postgrad.manchester.ac.uk>
2017-12-13 10:20:28 +0100

    input file

--- psf_check.py
+++ psf_check.py
@@ -1,11 +1,43 @@
 import os, sys
 
 ### Inputs
-scale = [0.001,0.01,0.1,1,10,100,1000]
-path_to_ms = '~/nas/radcliff/eMERGE/eMERLIN_L_Band/UV_data/eMERGE_DR1_PEELED/'
-path_to_casa = 'casa-release-5.1.1-5.el7/bin/'
-ms1 = 'eMER_L_25JUL2015_UVFIX_PEELED.ms'
-ms2 = 'eMER_L_17JUL2015_UVFIX_PEELED.ms'
+def headless(inputfile):
+    ''' Parse the list of inputs given in the specified file. (Modified from evn_funcs.py)'''
+    INPUTFILE = open(inputfile, "r")
+    control = {}
+    # a few useful regular expressions
+    newline = re.compile(r'\n')
+    space = re.compile(r'\s')
+    char = re.compile(r'\w')
+    comment = re.compile(r'#.*')
+    # parse the input file assuming '=' is used to separate names from values
+    for line in INPUTFILE:
+        if char.match(line):
+            line = comment.sub(r'', line)
+            line = line.replace("'", '')
+            (param, value) = line.split('=')
+            param = newline.sub(r'', param)
+            param = param.strip()
+            param = space.sub(r'', param)
+            value = newline.sub(r'', value)
+            value = value.replace(' ','').strip()
+            valuelist = value.split(',')
+            if len(valuelist) == 1:
+                if valuelist[0] == '0' or valuelist[0]=='1' or valuelist[0]=='2':
+                    control[param] = int(valuelist[0])
+                else:
+                    control[param] = str(valuelist[0])
+            else:
+                control[param] = ','.join(valuelist)
+    return control
+
+inputs = headless('catalog_inputs.txt')
+
+scale = float(inputs['scale'])
+path_to_ms = str(inputs['path_to_ms'])
+path_to_casa = str(inputs['path_to_casa'])
+ms1 = str(inputs['ms1'])
+ms2 = str(inputs['ms2'])
 
 os.system('rsync -ar --progress %s%s ./' % (path_to_ms,ms2))
 for i in range(len(scale)):
--- psf_weighting_plot.py
+++ psf_weighting_plot.py
@@ -3,6 +3,44 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
 
+def headless(inputfile):
+    ''' Parse the list of inputs given in the specified file. (Modified from evn_funcs.py)'''
+    INPUTFILE = open(inputfile, "r")
+    control = {}
+    # a few useful regular expressions
+    newline = re.compile(r'\n')
+    space = re.compile(r'\s')
+    char = re.compile(r'\w')
+    comment = re.compile(r'#.*')
+    # parse the input file assuming '=' is used to separate names from values
+    for line in INPUTFILE:
+        if char.match(line):
+            line = comment.sub(r'', line)
+            line = line.replace("'", '')
+            (param, value) = line.split('=')
+            param = newline.sub(r'', param)
+            param = param.strip()
+            param = space.sub(r'', param)
+            value = newline.sub(r'', value)
+            value = value.replace(' ','').strip()
+            valuelist = value.split(',')
+            if len(valuelist) == 1:
+                if valuelist[0] == '0' or valuelist[0]=='1' or valuelist[0]=='2':
+                    control[param] = int(valuelist[0])
+                else:
+                    control[param] = str(valuelist[0])
+            else:
+                control[param] = ','.join(valuelist)
+    return control
+
+inputs = headless('catalog_inputs.txt')
+
+scale = float(inputs['scale'])
+path_to_ms = str(inputs['path_to_ms'])
+path_to_casa = str(inputs['path_to_casa'])
+ms1 = str(inputs['ms1'])
+ms2 = str(inputs['ms2'])
+
 major = []
 minor = []
 weighting = []
@@ -15,9 +53,9 @@ for file in os.listdir('./'):
 	if file.endswith('.psf'):
 		name = file
 		print file
-		weight2 = name[name.find("ms1_")+4:name.find("_ms2_")]
+		weight2 = name[name.find("%s_" ms1.split('.ms')[0])+4:name.find("_%s_" % ms2.split('.ms')[0])]
 		print weight2
-		weighting = weighting + [float(name[name.find("ms1_")+4:name.find("_ms2_")])]
+		weighting = weighting + [float(name[name.find("%s_" ms1.split('.ms')[0])+4:name.find("_%s_" % ms2.split('.ms')[0])])]
 		x = imhead(file)
 		minor = minor + [x['restoringbeam']['major']['value']]
 		major = major + [x['restoringbeam']['minor']['value']]
@@ -28,12 +66,7 @@ for file in os.listdir('./'):
 
 print major, minor, weighting
 rms = rms*1E6
-def func(x,a,c,d):
-	return a*np.exp(-c*x)+d
 
-#popt, pcov = curve_fit(func,weighting,minor)
-#xx = np.linspace(0,200,1000)
-#yy = func(xx,*popt)
 plt.figure(1)
 plt.scatter(weighting,minor,c='b',label='minor axis')
 plt.scatter(weighting,major,c='g',label='major axis')
