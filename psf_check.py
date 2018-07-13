15e34f7e5752c370bdb53ded8733aa33c5c1cdfc
Jack Radcliffe <jack.radcliffe@postgrad.manchester.ac.uk>
2018-07-13 09:54:05 +0100

    added cell size changes

--- psf_check.py
+++ psf_check.py
@@ -35,6 +35,7 @@ try:
     path_to_ms1 = str(inputs['path_to_ms1'])
     path_to_ms2 = str(inputs['path_to_ms2'])
     path_to_casa = str(inputs['path_to_casa'])
+    adjust_cellsize = bool(inputs['adjust_cellsize'])
     if ',' in str(inputs['ms2']):
         ms2 = inputs['ms2'].split(',')
         ms2_name = '_'.join([i.split('.ms')[0] for i in ms2])
@@ -46,6 +47,12 @@ try:
 
     ms1 = str(inputs['ms1'])
     logging.info('Will scale %s by the following scales: %s' % (ms1, scale))
+    if ',' in str(inputs['psf_cell']):
+        cellsize = inputs['psf_cell'].split(',')
+        cellsize = [str(inputs['psf_cell'])+'arcsec' for i in cellsize]
+    else:
+        cellsize = [str(inputs['psf_cell'])+'arcsec']
+    print scale
     cellsize = str(inputs['psf_cell'])+'arcsec'
     imsize = str(int(inputs['psf_imsize']))
     phase_center = ' '.join(inputs['phase_center'].split(','))
@@ -56,17 +63,35 @@ try:
             os.system('rsync -ar --progress %s%s ./' % (path_to_ms2,i))
     else:
         os.system('rsync -ar --progress %s%s ./' % (path_to_ms2,ms2))
-    for i in range(len(scale)):
-        if os.path.exists('%s_%s_%s_1_psf_CASA.psf' % (ms1.split('.ms')[0],scale[i],ms2_name)) == False:
+
+    if adjust_cellsize == False:
+        cellsize = cellsize[0]
+        for i in range(len(scale)):
+            if os.path.exists('%s_%s_%s_1_psf_CASA.psf' % (ms1.split('.ms')[0],scale[i],ms2_name)) == False:
+                os.system('rsync -ar --progress %s%s ./' % (path_to_ms1,ms1))
+                for file in os.listdir('./'):
+                    if file==ms1:
+                        logging.info('Scaling %s by %.2f' % (ms1,scale[i]))
+                        os.system('%scasa --nologger --log2term -c wt_mod_CASAv2.py scale %s %s' % (path_to_casa,file, str(scale[i])))
+
+                logging.info('Imaging %s (scale %.2f) and %s' % (ms1,scale[i],ms2_inp))
+                os.system('%smpicasa -n %s %scasa --nologger --log2term -c tclean.py %s %s \'%s\' %s %s %s' % (path_to_casa, ncore, path_to_casa,cellsize,imsize,phase_center,str(scale[i]),ms1,ms2_inp))
+                os.system('rm casa*log')
+    elif adjust_cellsize == True:
+        scale = scale[0]
+        if os.path.exists('%s_%s_%s_1_psf_CASA.psf' % (ms1.split('.ms')[0],cellsize[i],ms2_name)) == False:
             os.system('rsync -ar --progress %s%s ./' % (path_to_ms1,ms1))
             for file in os.listdir('./'):
                 if file==ms1:
                     logging.info('Scaling %s by %.2f' % (ms1,scale[i]))
                     os.system('%scasa --nologger --log2term -c wt_mod_CASAv2.py scale %s %s' % (path_to_casa,file, str(scale[i])))
-
-            logging.info('Imaging %s (scale %.2f) and %s' % (ms1,scale[i],ms2_inp))
-            os.system('%smpicasa -n %s %scasa --nologger --log2term -c tclean.py %s %s \'%s\' %s %s %s' % (path_to_casa, ncore, path_to_casa,cellsize,imsize,phase_center,str(scale[i]),ms1,ms2_inp))
+        for i in range(len(cellsize)):
+            logging.info('Imaging %s (cellsize %s) and %s' % (ms1,cellsize[i],ms2_inp))
+            os.system('%smpicasa -n %s %scasa --nologger --log2term -c tclean.py %s %s \'%s\' %s %s %s' % (path_to_casa, ncore, path_to_casa,cellsize[i],imsize,phase_center,scale,ms1,ms2_inp))
             os.system('rm casa*log')
+
+    else:
+        print('Rubbish')
 
     gmail_emailer(user=user,pwd=pwd,recipient='j.f.radcliffe@rug.nl',subject='CODE %s RUN SUCCESSFULLY - %s' % (os.path.basename(__file__),platform.node()),\
     body='The code %s has run successfully on %s. \n Please see %s:%s  for the results.\n\n The logger output of %s is as follows:\n\n %s' % (os.path.basename(__file__),datetime.now() - startTime, platform.node(),os.path.dirname(os.path.realpath(__file__)), log_name, open(log_name,'r').read()))
